14c4f7b5edb27d3819d668468031c214b933e665
Isaiah Auguste <isaugust25@gmail.com>
2025-08-04 12:49:11 -0400

    Registered new admin and moderation routes in server entry

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
@@ -7,7 +8,7 @@ const app = express();
 const corsOptions = {
   origin: 'http://localhost:5500',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type']
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
@@ -21,6 +22,7 @@ const userRoutes = require('./routes/user');
 const genreRoutes = require('./routes/genres');
 const managerRoutes = require('./routes/manager');
 const moderationRoutes = require('./routes/moderation');
+const adminRoutes = require('./routes/admin'); 
 
 // Register routes
 app.use('/api/auth', authRoutes);
@@ -29,7 +31,8 @@ app.use('/api/mood', moodRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/genres', genreRoutes);        
 app.use('/api/manager', managerRoutes);
-app.use('/api/moderation', moderationRoutes);
+app.use('/api/moderation', moderationRoutes); 
+app.use('/api/admin', adminRoutes);
 
 
 const PORT = process.env.PORT || 3000;
