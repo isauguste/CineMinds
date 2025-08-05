40ef758e605d1511783f6035ac7a462d1650df0a
Isaiah Auguste <isaugust25@gmail.com>
2025-08-05 17:25:57 -0400

    Updated reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const reviewController = require('../controllers/reviewController');
+const reviewController = require('../controllers/reviewsController');
 const { authenticateToken, requireManager } = require('../utils/authMiddleware');
 
 // GET all pending reviews (Manager only)
