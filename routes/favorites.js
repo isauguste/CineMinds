1d015982b25f9b686c62ac25e604bf777324ed08
Isaiah Auguste <isaugust25@gmail.com>
2025-08-06 09:41:56 -0400

    Added routes to get all favorites and to un-favorite a movie.

--- routes/favorites.js
+++ routes/favorites.js
@@ -3,7 +3,15 @@ const router = express.Router();
 const favoritesController = require('../controllers/favoritesController');
 const { authenticateToken } = require('../utils/authMiddleware');
 
-router.post('/', authenticateToken, favoritesController.saveToFavorites);
+// Add a favorite
+router.post('/', authenticateToken, favoritesController.addFavorite);
+
+// Get all favorites for the logged-in user
+router.get('/', authenticateToken, favoritesController.getFavorites);
+
+// Delete a favorite by movie ID
+router.delete('/:movieId', authenticateToken, favoritesController.deleteFavorite);
 
 module.exports = router;
+
 
