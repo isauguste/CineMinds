2671c7b0f075437190101042917d10d71ce9275e
Isaiah Auguste <isaugust25@gmail.com>
2025-08-08 20:06:59 -0400

    Updated availability route for /api/availabilty endpoint.

--- routes/availability.js
+++ routes/availability.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const controller = require("../controllers/availabilityController");
 
+// GET /api/availability
 router.get("/", controller.getAvailability);
 
 module.exports = router;
